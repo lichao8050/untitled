8bb219eabbed2b335210d108d38aaaae89d56e2b
lichao <lichao8050@outlook.com>
2022-09-20 16:51:55 +0800

    上传11

--- commons/request_page.py
+++ commons/request_page.py
@@ -4,6 +4,8 @@
 # @FileName : request_page.py
 import requests
 import time
+import json
+import jsonpath
 
 
 class HttpRequest:
@@ -19,6 +21,15 @@ class HttpRequest:
         elif method == 'post':
             res = HttpRequest.session.request(method, url, **kwargs)
         return res
+
+    #  基于jsonpath获取数据的关键字：用于提取所需要的内容
+    def get_text(self, data, key):
+        """jsonpath获取数据的表达式：成功则返回list，失败则返回false
+         loads是将json格式的内容转换为字典的格式
+         jsonpath接收的是dict类型的数据"""
+        dict_data = json.loads(data)
+        value = jsonpath.jsonpath(dict_data, '$..{0}'.format(key))
+        return value[0]
 
     def get_random(self):
         return str(int(time.time()))
--- commons/request_until.py
+++ commons/request_until.py
@@ -2,29 +2,23 @@
 # @Time     : 2022/9/15 10:19
 # @Author   : Mr_Li
 # @FileName : commons.py
-
+from py_excl.read_excel import ExcelReader
 import requests
 from commons.request_page import HttpRequest
 from commons.yaml_until import YamlUntil
 import pytest
 
 
-# class TestRequest:
-#     print(50 * '-' + "开始测试" + 50 * '-')
-#
-#     def setup(self):
-#         print("每个用例执行之前的操作")
-#
-#     def teardown(self):
-#         self.session.close()
-#         try:
-#             del self.session.cookies['JSESSIONID']
-#         except:
-#             pass
-#         print("每个用例执行之后的操作")
-
-    # def setup_class(self):
-    #     print("每个类执行之前的操作")
-    #
-    # def teardown_class(self):
-    #     print("每个类执行之前的操作")
+@pytest.fixture(scope='session', autouse=True)
+def start_test():
+    # 定义读取表格变量excel
+    excel = ExcelReader(r"D:\untitled\py_excl\login_excel.xlsx")
+    #  定义统计所有列的变量count_raw
+    count_row = excel.get_case_count()
+    for row in range(0, count_row):  # 循环语句   row是变量  范围是0到count
+        test_casediscrible = excel.get_test_case_describe(row)
+        url = excel.get_url(row)
+        data = eval(excel.get_request_data(row))  # 此处参数类型不是字符串，所以用eval转换为字典类型
+        method = excel.get_method(row)
+        headers = eval(excel.get_headers(row))  # 此处参数类型不是字符串，所以用eval转换为字典类型
+        res = HttpRequest().send_all_request(method=method, headers=headers, url=url, params=data)
